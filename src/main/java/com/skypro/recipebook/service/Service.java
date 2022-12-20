f5acd0a945df450a73d74e0535fa972a51864491
BULAT <ibragimovbi@yandex.ru>
2022-12-20 18:26:54 +0300

    Исправил замечания кроме обработки исключений

--- src/main/java/com/skypro/recipebook/service/Service.java
+++ src/main/java/com/skypro/recipebook/service/Service.java
@@ -2,8 +2,10 @@ package com.skypro.recipebook.service;
 
 import com.skypro.recipebook.model.Recipe;
 
+import javax.naming.NameNotFoundException;
+
 public interface Service {
     String add(Object obj);
 
-    Object get(int id);
+    Object get(int id) throws NameNotFoundException;
 }
