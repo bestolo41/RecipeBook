3cb5971501f3c6c46ec9e33c209cef9cb730b959
BULAT <ibragimovbi@yandex.ru>
2022-12-22 23:09:41 +0300

    ДЗ по API и Postman

--- src/main/java/com/skypro/recipebook/service/RecipeService.java
+++ src/main/java/com/skypro/recipebook/service/RecipeService.java
@@ -1,9 +1,23 @@
 package com.skypro.recipebook.service;
 
+import com.skypro.recipebook.model.Ingredient;
 import com.skypro.recipebook.model.Recipe;
+
+import java.util.HashSet;
+import java.util.LinkedList;
 
 public interface RecipeService {
     public String add(Recipe recipe);
 
     public Recipe get(int id);
+
+    String edit(int id, Recipe newRecipe);
+
+    String delete(int id);
+
+    HashSet<Recipe> getRecipesByIngredientId(int id);
+
+    HashSet<Recipe> findRecipeByFewIngredients(LinkedList<Ingredient> ingredients);
+
+    LinkedList<Recipe> recipesPageByPage(int page);
 }
--- src/main/java/com/skypro/recipebook/service/impl/RecipeServiceImpl.java
+++ src/main/java/com/skypro/recipebook/service/impl/RecipeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.skypro.recipebook.service.impl;
 
+import com.skypro.recipebook.model.Ingredient;
 import com.skypro.recipebook.model.NotFoundException;
 import com.skypro.recipebook.model.ReAddingException;
 import com.skypro.recipebook.model.Recipe;
@@ -7,8 +8,10 @@ import com.skypro.recipebook.service.RecipeService;
 import org.springframework.stereotype.Service;
 
 
-import javax.naming.NameNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 @Service
 public class RecipeServiceImpl implements RecipeService {
@@ -33,6 +36,83 @@ public class RecipeServiceImpl implements RecipeService {
         } else {
             throw new NotFoundException("Рецепта с таким id не существует");
         }
+    }
+
+    @Override
+    public String edit(int id, Recipe newRecipe) {
+        if (recipeBook.containsKey(id)) {
+            Recipe oldRecipe = recipeBook.get(id);
+            recipeBook.put(id, newRecipe);
+            return "Рецепт " + oldRecipe.getName() + " обновлен";
+        }
+        throw new NotFoundException("Рецепта с таким id не существует");
+    }
+
+    @Override
+    public String delete(int id) {
+        if (recipeBook.containsKey(id)) {
+            recipeBook.remove(id);
+            return "Рецепт с id " + id + " удален";
+        }
+        throw new NotFoundException("Рецепта с таким id не существует");
+    }
+
+    @Override
+    public HashSet<Recipe> getRecipesByIngredientId(int id) {
+        HashSet<Recipe> recipesContainsIngredientWithId = new HashSet<>();
+        String ingredientName;
+
+        if (IngredientServiceImpl.getIngredients().containsKey(id)) {
+            ingredientName = IngredientServiceImpl.getIngredients().get(id).getName();
+        } else {
+            throw new NotFoundException("Ингредиента с таким id не существует");
+        }
+
+        for (Recipe recipe : recipeBook.values()) {
+            for (Ingredient ingredient : recipe.getIngredients()) {
+                if (ingredient.getName().equals(ingredientName)) {
+                    recipesContainsIngredientWithId.add(recipe);
+                    break;
+                }
+            }
+        }
+
+        if (recipesContainsIngredientWithId.isEmpty()) {
+            throw new NotFoundException("Рецептов, содежащих ингредиент с таким id нет");
+        }
+        return recipesContainsIngredientWithId;
+    }
+
+    @Override
+    public HashSet<Recipe> findRecipeByFewIngredients(LinkedList<Ingredient> ingredients) {
+        HashSet<Recipe> recipesContainsFewIngredients = new HashSet<>();
+
+        for (Recipe recipe : recipeBook.values()) {
+            if (recipe.getIngredients().containsAll(ingredients)) {
+                recipesContainsFewIngredients.add(recipe);
+            }
+        }
+
+        if (recipesContainsFewIngredients.isEmpty()) {
+            throw new NotFoundException("Рецептов с такими ингредиентами нет");
+        }
+        return recipesContainsFewIngredients;
+    }
+
+    @Override
+    public LinkedList<Recipe> recipesPageByPage(int page) {
+        int countOnPage = 10;
+        LinkedList<Recipe> recipesForResponse = new LinkedList<>();
+        Object[] allRecipes = recipeBook.values().toArray();
+
+        for (int i = (page - 1) * countOnPage; i < page * countOnPage - 1; i++) {
+            if (i >= allRecipes.length || i < 0) {
+                break;
+            } else if (allRecipes[i] != null) {
+                recipesForResponse.add((Recipe) allRecipes[i]);
+            }
+        }
+        return recipesForResponse;
     }
 
 }
