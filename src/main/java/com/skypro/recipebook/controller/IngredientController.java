5a8f6b92b7b455d31979c2582386f2a787770b8b
BULAT <ibragimovbi@yandex.ru>
2023-01-13 19:31:20 +0300

    ДЗ по библиотекам

--- src/main/java/com/skypro/recipebook/controller/IngredientController.java
+++ src/main/java/com/skypro/recipebook/controller/IngredientController.java
@@ -4,11 +4,14 @@ import com.skypro.recipebook.model.Ingredient;
 import com.skypro.recipebook.model.Recipe;
 import com.skypro.recipebook.service.IngredientService;
 import com.skypro.recipebook.service.impl.IngredientServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ingredients")
+@Tag(name = "Ингредиенты", description = "Операции над ингредиентами")
 public class IngredientController {
 
     IngredientService ingredientService;
@@ -18,20 +21,36 @@ public class IngredientController {
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Получить ингредиент по id",
+            description = "Введите номер id ингредиента чтобы получить его"
+    )
     public ResponseEntity<Ingredient> getIngredient(@PathVariable int id) {
         return ResponseEntity.ok(ingredientService.get(id));
     }
 
     @PostMapping
+    @Operation(
+            summary = "Добавить новый ингредиент",
+            description = "Укажите новый интредиент в формате JSON чтобы добавить его"
+    )
     public ResponseEntity<String> addIngredient(@RequestBody Ingredient ingredient) {
         return ResponseEntity.ok(ingredientService.add(ingredient));
     }
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Изменить ингредиент по id",
+            description = "Введите номер id ингредиента чтобы изменить его. Введите новые поля в формате JSON"
+    )
     public ResponseEntity<String> editIngredient(@PathVariable int id, @RequestBody Ingredient newIngredient) {
         return ResponseEntity.ok(ingredientService.edit(id, newIngredient));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Удалить ингредиент по id",
+            description = "Введите номер id ингредиента чтобы удалить его"
+    )
     public ResponseEntity<String> deleteIngredient(@PathVariable int id) {
         return ResponseEntity.ok(ingredientService.delete(id));
     }
--- src/main/java/com/skypro/recipebook/model/Ingredient.java
+++ src/main/java/com/skypro/recipebook/model/Ingredient.java
@@ -1,7 +1,12 @@
 package com.skypro.recipebook.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
 import java.util.Objects;
 
+@Getter
+@EqualsAndHashCode
 public class Ingredient {
     private String name;
     private int count;
@@ -13,20 +18,12 @@ public class Ingredient {
         setMeasureUnit(measureUnit);
     }
 
-    public String getName() {
-        return name;
-    }
-
     public void setName(String name) {
         if (name == null || name.isBlank() || name.isEmpty()) {
             throw new RuntimeException("Пустое название ингредиента");
         } else {
             this.name = name;
         }
-    }
-
-    public int getCount() {
-        return count;
     }
 
     public void setCount(int count) {
@@ -37,25 +34,8 @@ public class Ingredient {
         }
     }
 
-    public String getMeasureUnit() {
-        return measureUnit;
-    }
-
     public void setMeasureUnit(String measureUnit) {
         this.measureUnit = measureUnit;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Ingredient that = (Ingredient) o;
-        return count == that.count && Objects.equals(name, that.name) && Objects.equals(measureUnit, that.measureUnit);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, count, measureUnit);
     }
 
     @Override
